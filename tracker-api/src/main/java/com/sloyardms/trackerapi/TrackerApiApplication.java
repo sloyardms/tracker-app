533a894e0e224f045ac0786a0e34dd711461f74a
sloyardms <sloyard_ms@outlook.com>
2025-08-01 15:16:36 -0700

    tracker-api project init

---
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/TrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sloyardms.trackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerApiApplication.class, args);
+    }
+
+}
