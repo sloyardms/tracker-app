c3bc301e015595660666de5fbf0972a65440f49b
sloyardms <sloyard_ms@outlook.com>
2025-08-03 00:03:14 -0700

    feat(user) add User repository

--- tracker-api/src/main/java/com/sloyardms/trackerapi/repository/UserRepository.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.sloyardms.trackerapi.repository;
 
+import com.sloyardms.trackerapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
 }
