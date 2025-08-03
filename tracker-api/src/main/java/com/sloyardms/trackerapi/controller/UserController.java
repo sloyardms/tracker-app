e35ec53d9e24609b7bb6b2a939868b7a6ccf21d9
sloyardms <sloyard_ms@outlook.com>
2025-08-03 01:21:20 -0700

    feat(user) refactoring UserController for changes done in service layer

--- tracker-api/src/main/java/com/sloyardms/trackerapi/controller/UserController.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/controller/UserController.java
@@ -3,7 +3,6 @@ package com.sloyardms.trackerapi.controller;
 import com.sloyardms.trackerapi.dto.UserCreateDto;
 import com.sloyardms.trackerapi.dto.UserDto;
 import com.sloyardms.trackerapi.dto.UserUpdateDto;
-import com.sloyardms.trackerapi.entity.User;
 import com.sloyardms.trackerapi.service.interfaces.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -27,38 +26,24 @@ public class UserController {
     }
 
     @GetMapping("/{uuid}")
-    public ResponseEntity<UserDto> getById(String uuid){
-        UserDto user = userService.findByUuid(UUID.fromString(uuid));
-        if(user != null) {
-            return ResponseEntity.ok(user);
-        }else{
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<UserDto> getById(@PathVariable UUID uuid){
+        return ResponseEntity.ok(userService.findByUuid(uuid));
     }
 
     @PostMapping
     public ResponseEntity<UserDto> create(@Valid @RequestBody UserCreateDto userDto){
-        UserDto createdUser = userService.create(userDto);
-        if(createdUser != null) {
-            return ResponseEntity.ok(createdUser);
-        }else{
-            return ResponseEntity.badRequest().build();
-        }
+        return ResponseEntity.ok(userService.create(userDto));
     }
 
     @PatchMapping("/{uuid}")
     public ResponseEntity<UserDto> update(@PathVariable UUID uuid, @RequestBody UserUpdateDto userDto){
-        UserDto updatedUser = userService.update(uuid,userDto);
-        if(updatedUser != null) {
-            return ResponseEntity.ok(updatedUser);
-        }else{
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(userService.update(uuid, userDto));
     }
 
     @DeleteMapping("/{uuid}")
-    public void delete(@PathVariable UUID uuid){
+    public ResponseEntity<Void> delete(@PathVariable UUID uuid){
         userService.delete(uuid);
+        return ResponseEntity.noContent().build();
     }
 
 }
