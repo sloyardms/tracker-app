50fe2bea8094ba94ebef8db6d2b291778b7972ff
sloyardms <sloyard_ms@outlook.com>
2025-08-04 02:38:10 -0700

    feat(user) change Auditable to use Instant for createAt and updatedAt

--- tracker-api/src/main/java/com/sloyardms/trackerapi/entity/Auditable.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/entity/Auditable.java
@@ -9,7 +9,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.OffsetDateTime;
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -19,10 +19,10 @@ public abstract class Auditable {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private OffsetDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private OffsetDateTime updatedAt;
+    private Instant updatedAt;
 
 }
