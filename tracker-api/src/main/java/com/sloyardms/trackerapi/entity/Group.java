6c1a0c81304456a13bf10eb1272bc98e67918ab0
sloyardms <sloyard_ms@outlook.com>
2025-08-05 13:55:15 -0700

    feat(group): add group entity

---
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/entity/Group.java
@@ -0,0 +1,35 @@
+package com.sloyardms.trackerapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_uuid", "name"})
+})
+public class Group extends Auditable{
+
+    @Id
+    @Column(name = "group_uuid", nullable = false, updatable = false)
+    private UUID uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_uuid", nullable = false)
+    private User user;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+}
