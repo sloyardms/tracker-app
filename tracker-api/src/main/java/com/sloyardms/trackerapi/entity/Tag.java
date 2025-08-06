312bb836050052df8d3d88243711d53e1b2907a5
sloyardms <sloyard_ms@outlook.com>
2025-08-05 22:52:36 -0700

    feat(tag): add Tag entity

---
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/entity/Tag.java
@@ -0,0 +1,36 @@
+package com.sloyardms.trackerapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tags", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_uuid", "name"})
+})
+public class Tag extends Auditable {
+
+    @Id
+    @Column(name = "tag_uuid", nullable = false, updatable = false)
+    private UUID uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_uuid", nullable = false)
+    private User user;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    // Read only FK to avoid fetching the user on every query
+    @Column(name = "user_uuid", nullable = false, insertable = false, updatable = false)
+    private UUID userUuid;
+
+}
