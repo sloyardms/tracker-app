c769e39f4129c227a0d0733ac4b9fdf1f4be5bc2
sloyardms <sloyard_ms@outlook.com>
2025-08-05 20:28:59 -0700

    feat(group): add DTOs for creating, updating, and fetching groups

---
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/dto/GroupUpdateDto.java
@@ -0,0 +1,19 @@
+package com.sloyardms.trackerapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupUpdateDto {
+
+    private String name;
+    private String description;
+
+}
