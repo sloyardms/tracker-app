1065a96f35363e7830f46c836bee093587e0e1c1
sloyardms <sloyard_ms@outlook.com>
2025-08-05 22:58:00 -0700

    feat(tag): add DTOs for creating, updating and fetching Tags

---
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/dto/TagCreateDto.java
@@ -0,0 +1,24 @@
+package com.sloyardms.trackerapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagCreateDto {
+
+    @NotNull
+    private UUID userUuid;
+
+    @NotBlank
+    private String name;
+
+}
