9cb6f1e8e581bd720e256a0ff49a1d87e12ea91d
sloyardms <sloyard_ms@outlook.com>
2025-08-07 01:16:00 -0700

    chore(tag): remove unused imports

--- tracker-api/src/main/java/com/sloyardms/trackerapi/dto/TagCreateDto.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/dto/TagCreateDto.java
@@ -1,13 +1,10 @@
 package com.sloyardms.trackerapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Getter
 @Setter
