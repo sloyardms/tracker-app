e444bdf3e7a8bd3271aca84c4ed973d5d7d00250
sloyardms <sloyard_ms@outlook.com>
2025-08-09 15:22:23 -0700

    refactor(bookmark) add mapping configuration for ignoring null values from BookmarkUpdateDto

--- tracker-api/src/main/java/com/sloyardms/trackerapi/bookmark/mapper/BookmarkMapper.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/bookmark/mapper/BookmarkMapper.java
@@ -12,11 +12,16 @@ public interface BookmarkMapper {
 
     BookmarkDto toDto(Bookmark bookmark);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, ignoreByDefault = true)
     @Mapping(target = "group", ignore = true)
     @Mapping(target = "tags", ignore = true)
     @Mapping(target = "notes", ignore = true)
     @Mapping(target = "images", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "userUuid", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateFromDto(BookmarkUpdateDto bookmarkUpdateDto, @MappingTarget Bookmark bookmark);
 
 }
