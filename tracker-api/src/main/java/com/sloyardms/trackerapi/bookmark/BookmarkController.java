cd3c89a0a23afbaf60ad3dd874fd8c9257a11ca3
sloyardms <sloyard_ms@outlook.com>
2025-08-09 20:52:51 -0700

    feat(bookmark): add BookmarkController methods to create notes associated with a bookmark

--- tracker-api/src/main/java/com/sloyardms/trackerapi/bookmark/BookmarkController.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/bookmark/BookmarkController.java
@@ -3,13 +3,18 @@ package com.sloyardms.trackerapi.bookmark;
 import com.sloyardms.trackerapi.bookmark.dto.BookmarkCreateDto;
 import com.sloyardms.trackerapi.bookmark.dto.BookmarkDto;
 import com.sloyardms.trackerapi.bookmark.dto.BookmarkUpdateDto;
+import com.sloyardms.trackerapi.note.dto.NoteCreateDto;
+import com.sloyardms.trackerapi.note.dto.NoteDto;
 import com.sloyardms.trackerapi.security.AuthUtils;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.UUID;
@@ -59,6 +64,25 @@ public class BookmarkController {
     public ResponseEntity<Void> delete(@PathVariable UUID uuid){
         bookmarkService.delete(uuid);
         return ResponseEntity.noContent().build();
+    }
+
+    // NOTE ENDPOINTS
+    @PostMapping("/{uuid}/notes")
+    public ResponseEntity<NoteDto> createNote(@PathVariable UUID uuid, @Valid @RequestBody NoteCreateDto noteCreateDto){
+        NoteDto savedNote = bookmarkService.createNote(uuid, noteCreateDto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{noteUuid}")
+                .buildAndExpand(savedNote.getUuid())
+                .toUri();
+        return ResponseEntity.created(location).body(savedNote);
+    }
+
+    @GetMapping("/{uuid}/notes")
+    public ResponseEntity<Page<NoteDto>> getAllNotesByBookmarkUuid(
+            @PathVariable UUID uuid,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<NoteDto> notes = bookmarkService.getAllNotesByBookmarkUuid(uuid, pageable);
+        return ResponseEntity.ok(notes);
     }
 
 }
