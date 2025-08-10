52f7136e40da671b8f387aabd38c9211230dfb24
sloyardms <sloyard_ms@outlook.com>
2025-08-09 20:47:07 -0700

    refactor: add NotBlank validation to GroupUpdateDto, TagUpdateDto and UserUpdateDto

--- tracker-api/src/main/java/com/sloyardms/trackerapi/group/dto/GroupUpdateDto.java
+++ tracker-api/src/main/java/com/sloyardms/trackerapi/group/dto/GroupUpdateDto.java
@@ -1,5 +1,6 @@
 package com.sloyardms.trackerapi.group.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class GroupUpdateDto {
 
+    @NotBlank
     private String name;
     private String description;
 
